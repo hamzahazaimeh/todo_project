9852f6409508edafad301c6bfb44c759e359808f
hamzahazaimeh <h.hazaimeh96@gmail.com>
2022-02-14 06:15:08 +0200

    j2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-var Username = prompt('Hi! what is  your name ?  ');
+var Username = prompt('Hi! what is your name?');
 
 
 var UserGender = prompt( 'what is your Gender ?(male or female )');
 
-var UserAge = prompt( ' Please Enter Your Age ');
+var UserAge = prompt( 'Please Enter Your Age');
 
 if(UserAge<=0){
-    alert( "the Age you entered less than or equal zero... ")
+    alert( "the Age you entered less than or equa l zero ")
 }
 
 var skipwelcomingmeesage = confirm("Do you want skip welcoming meesage ..")
@@ -24,7 +24,16 @@ if(skipwelcomingmeesage == false){
     }
     else
     {
-        alert("welcom " + Username)
+        alert("welcome " + Username)
     }
 
 }
+
+
+var UserQuestion = prompt( 'Now you should answer to three question  press skip to start ');
+var Question1 = prompt( 'Are you student ? ');
+var Question2 = prompt( 'Are you living in jordan ? ');
+var Question3 = prompt( 'Are you living with your family ? ');
+
+var answer = [Question1+Question2+Question3]
+console.log(answer);
